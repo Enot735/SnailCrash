7876a1f4bf61e9e0489fdfaf7ca06c60d4396e82
Enot735 <kotiksv19@gmail.com>
2021-09-18 23:04:23 +1000

    mechanics-2
    Добавлен новый способ (1-ая реализация) передвижения "брошенного" объекта - по точкам (соединенных отрисованной линией).
    В данной механике движение возможно только по прямым, можно сгладить большим количеством точек.
    Игрок не учтен в данной реализации.

---
+++ Assets/Scripts/FollowPath.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    // Используемый путь
+    public MovementPath MyPath;
+
+    // Скорость движения
+    public float speed = 1;
+
+    // На какое расстояние должен подъехать объект к точке,
+    // чтобы распознать, что это точка
+    public float maxDistance = .1f;
+
+    // Проверка точек
+    private IEnumerator<Transform> pointInPath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Проверка - прикрепили ли мы путь
+        if (MyPath == null)
+        {
+            Debug.Log("Необходимо применить путь");
+            return;
+        }
+
+        // Обращение к корутине
+        // есть точка или нет
+        pointInPath = MyPath.GetNextPathPoint();
+
+        // Получение следующей точки пути
+        pointInPath.MoveNext();
+
+        //  есть ли точка, к которой передвигаться
+        if (pointInPath.Current == null)
+        {
+            Debug.Log("Необходимо добавить точки в путь");
+            return;
+        }
+
+        // Объект помещается на первую точку пути
+        transform.position = pointInPath.Current.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Провека отсутствия пути
+        if (pointInPath == null || pointInPath.Current == null)
+        {
+            return;
+        }
+
+        // Движение объекта к следующей точке
+        transform.position = Vector3.MoveTowards(transform.position, pointInPath.Current.position, Time.deltaTime * speed);
+
+        // Проверка - достаточно ли мы близко к точке, чтобы начать движение к следующей точке
+        var distanceSquare = (transform.position - pointInPath.Current.position).sqrMagnitude;
+
+        // Достаточно ли мы близко
+        // Теорема Пифагора
+        if (distanceSquare < maxDistance * maxDistance)
+        {
+            // Двигаемся к следующей точке
+            pointInPath.MoveNext();
+        }
+
+    }
+}
---
+++ Assets/Scripts/MovementPath.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPath : MonoBehaviour
+{
+    // направление движения
+    // 1 - вперед, по часовой
+    // -1 - назад, против часовой
+    public int movementDirection = 1;
+
+    // к какой точке двигаться
+    public int movingTo = 0;
+
+    public Transform[] pathElements;
+
+    public void OnDrawGizmos()
+    {
+        // проверка пути на пустоту
+        if (pathElements == null || pathElements.Length < 2)
+        {
+            return;
+        }
+
+        for (int i = 1; i < pathElements.Length; i++)
+        {
+            Gizmos.DrawLine(pathElements[i - 1].position, pathElements[i].position);
+        }
+    }
+
+    public IEnumerator<Transform> GetNextPathPoint()
+    {
+        if (pathElements == null || pathElements.Length < 1)
+        {
+            // Выход из корутины
+            yield break;
+        }
+
+        while (true)
+        {
+            // Возвращает текущее положение точки
+            yield return pathElements[movingTo];
+
+            // Если точка всего одна, выходим
+            if (pathElements.Length == 1)
+            {
+                continue;
+            }
+
+            // Если двгигаемся по нарастающей
+            if (movingTo <= 0)
+            {
+                movementDirection = 1;
+            }
+            // Если двигаемся по убывающей
+            else if (movingTo >= pathElements.Length - 1)
+            {
+                movementDirection = -   1;
+            }
+
+            // Диапазон движений от 1 до -1
+            movingTo += movementDirection;
+        }
+    }
+}
